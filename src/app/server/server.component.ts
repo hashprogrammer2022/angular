afe679515cb36fa77e1bbd99ea9ea087abd0c335
Hicham-dine Ahmed <programming.training2022@gmail.com>
2023-05-16 02:27:59 +0300

    Course progress up to video 36 of the angular basics

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
     styleUrls:['./server.component.css']
 })
 export class ServerComponent{
-
+    name = "massiwani - server";
+    id = 10;
+    status = 'online';
 }
